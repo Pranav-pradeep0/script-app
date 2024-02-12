bff2b921ff74327c1ab7c75888469ea662215311
mnz <muneeskarakkad62@gmail.com>
2024-02-12 11:19:40 +0530

    login password visible and non visible toggle completed

--- frontend/src/components/common/loginForm/LoginForm.jsx
+++ frontend/src/components/common/loginForm/LoginForm.jsx
@@ -1,4 +1,11 @@
-import { FormLabel, Stack, Typography, useTheme } from '@mui/material';
+import {
+  FormLabel,
+  IconButton,
+  InputAdornment,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
@@ -8,36 +15,36 @@ import { useState } from 'react';
 import { VITE_BASE_URL } from '@/constants';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const LoginForm = () => {
-
   const { palette } = useTheme();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const [inputType, setInputType] = useState('password');
   const [loginCreds, setLoginCreds] = useState({
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
   const onLoginInputsChange = (e) => {
-    const { value, name } = e.target
-    const existingData = loginCreds
-    setLoginCreds({ ...existingData, [name]: value })
-  }
+    const { value, name } = e.target;
+    const existingData = loginCreds;
+    setLoginCreds({ ...existingData, [name]: value });
+  };
 
   const handleLogin = async () => {
     try {
       const response = await axios.post(`${VITE_BASE_URL}/auth/login`, {
         email: loginCreds.email,
-        password: loginCreds.password
+        password: loginCreds.password,
       });
 
       if (response.status === 200) {
         const userId = response.data.user._id;
-        const userName = response.data.user.firstname
+        const userName = response.data.user.firstname;
         localStorage.setItem('userId', userId);
-        localStorage.setItem('userName', userName)
+        localStorage.setItem('userName', userName);
         dispatch(setLogin(true));
 
         Swal.fire({
@@ -50,7 +57,6 @@ const LoginForm = () => {
         setTimeout(() => {
           navigate('/');
         }, 1000);
-
       } else {
         console.error('Login failed');
         console.log(response.data.message);
@@ -65,12 +71,39 @@ const LoginForm = () => {
     }
   };
 
+  const handleClickShowPassword = () => {
+    setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <Stack component="form" gap="0.94rem" width="100%">
       <Typography variant="h2">Sign In</Typography>
-      <AuthInput placeholder="Email" value={loginCreds.email} name='email' onChange={onLoginInputsChange} />
-      <AuthInput placeholder="Password" type="password" value={loginCreds.password} name='password' onChange={onLoginInputsChange} />
+      <AuthInput
+        placeholder="Email"
+        value={loginCreds.email}
+        name="email"
+        onChange={onLoginInputsChange}
+      />
+      <div style={{ position: 'relative' }}>
+        <AuthInput
+          sx={{ position: 'relative' }}
+          placeholder="Password"
+          type={inputType}
+          value={loginCreds.password}
+          name="password"
+          onChange={onLoginInputsChange}
+        ></AuthInput>
+        <IconButton sx={{position:"absolute",right:"15px",top:"5px"}}
+          onClick={handleClickShowPassword}
+          onMouseDown={handleMouseDownPassword}
+        >
+          {inputType === 'password' ? <VisibilityOff /> : <Visibility />}
+        </IconButton>
+      </div>
       <Link style={{ textDecoration: 'none' }} to="/login">
         <FormLabel
           sx={{
